6264e932892ca1b45d74eab03c3250cee9e4ecae
gaurishmalhotra <gaurishmalhotra2005@gmail.com>
2024-08-19 19:09:06 +0530

    Create invertstar.cpp

---
+++ invertstar.cpp
@@ -0,0 +1,24 @@
+//Gaurish Malhotra
+//23070123054
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int i,j;
+    for( i=1;i<=5;i++) 
+    {
+        for(j=5;j>=i;j--)
+        {
+            cout<<" *"<<" ";
+        }
+ cout<<" \n";
+    }}
+    //output
+    /* 
+*  *  *  *  *  
+ *  *  *  *
+ *  *  *
+ *  *
+ *
+ */
