1ce3046e18be828939c7ce5b4c5d416ccd87860c
gaurishmalhotra <gaurishmalhotra2005@gmail.com>
2024-08-19 19:08:11 +0530

    Create forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,18 @@
+//Gaurish Malhotra
+//23070123054
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+     int a;
+    for(a=0;a<=5;a++)
+    cout<<"SIT"<<endl;
+}
+//ouput
+/*SIT
+SIT
+SIT
+SIT
+SIT
+SIT*/
